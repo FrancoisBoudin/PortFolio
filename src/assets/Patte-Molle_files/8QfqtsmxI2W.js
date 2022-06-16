b36e7f5902c01ca946cf645328ac02792db05a9c
FrancoisBoudin <Boudinfrancois@hotmail.com>
2022-06-16 11:32:21 +0200

    habillage de la page avec les skills

---
+++ src/assets/Patte-Molle_files/8QfqtsmxI2W.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }/*FB_PKG_DELIM*/
+
+__d("LSIntEnum",["I64"],(function(a,b,c,d,e,f,g){"use strict";function a(a){return d("I64").of_float(a)}function b(a){return d("I64").to_float(a)}g.ofNumber=a;g.toNumber=b}),98);
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,21 @@
-import './home.css';
+import './home.scss';
 
 const Home = () => (
   <div className="home-page">
-    <h1 className="titre">Boudin François</h1>
-    <div>
-      <p className="sous-titre">Bonjour et bienvenue sur mon portfolio .</p>
+    <div className="header">
+      <h1 className="title">Boudin François</h1>
+      <p className="under-title">Développeur Frontend React</p>
+    </div>
+    <div className="div-skill">
+      <h2 className="title-skill">SKILLS</h2>
+      <div className="pictures-skills">
+        <img className="html" data-text="HTML" src="https://cdn.discordapp.com/attachments/518594672122134538/986918721006207006/html-5.png" alt="logo-html" />
+        <img data-text="CSS" src="https://cdn.discordapp.com/attachments/518594672122134538/986918747874922526/css-3.png" alt="" />
+        <img data-text="Javascript" src="https://cdn.discordapp.com/attachments/518594672122134538/986918720435793980/js.png" alt="" />
+        <img data-text="React.js" src="https://cdn.discordapp.com/attachments/518594672122134538/986918720700047400/react.png" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
     </div>
   </div>
 );
